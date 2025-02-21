36f340eb95d9844302fa00b2babc2f1068077de4
matyii <kmatyas2004@gmail.com>
2025-02-21 22:57:42 +0100

    beta release of the v1 homepage

--- assets/app.js
+++ assets/app.js
@@ -20,23 +20,89 @@ $(document).ready(function() {
 
     const currentYear = new Date().getFullYear();
     $('.currentYear').text(currentYear);
-});
 
-// animations
-ScrollReveal({ distance: '15px', reset: true });
 
-const scrollRevealConfigs = [
-    { selector: '.navbar', options: { delay: 100, duration: 500, origin: 'top' } },
-    { selector: '#hero-image', options: { delay: 400, duration: 500, origin: 'right' } },
-    { selector: '#hero-text', options: { delay: 700, duration: 800, origin: 'left' } },
-    { selector: '#mockup-image', options: { delay: 400, duration: 500, scale: 0.2 } },
-    { selector: '#mockup-text', options: { delay: 700, duration: 1200, origin: 'right' } },
-    { selector: '#install', options: { delay: 300, duration: 500, scale: 0 } },
-    { selector: '#discord', options: { delay: 300, duration: 500, origin: 'bottom' } },
-    { selector: '#macbook_mockup_discord', options: { delay: 800, duration: 500 } },
-    { selector: '.footer', options: { delay: 100, duration: 500, origin: 'bottom' } }
-];
+    // typing animations
+    var typed = new Typed('#install-typed', {
+        "strings": [
+            "simple.",
+            "quick.",
+            "lightweight.",
+            "readable.",
+            "straightforward.",
+            "effortless.",
+            "uncomplicated.",
+            "minimalistic.",
+            "elegant."
+        ],
+        typeSpeed: 90,
+        backDelay: 2000,
+        backSpeed: 70,
+        showCursor: false,
+        shuffle: true,
+        loop: true,
+        loopCount: Infinity,
+    });
 
-scrollRevealConfigs.forEach(config => {
-    ScrollReveal().reveal(config.selector, config.options);
+    var typed = new Typed('#sharex-typed', {
+        "strings": [
+            "we host.",
+            "we serve it.",
+            "we do the job.",
+            "we share it.",
+            "we deliver.",
+            "we take care of it.",
+            "we handle it.",
+            "we make it available."
+        ],
+        typeSpeed: 90,
+        backDelay: 2000,
+        backSpeed: 70,
+        showCursor: false,
+        shuffle: true,
+        loop: true,
+        loopCount: Infinity,
+    });
+
+    var typed = new Typed('#questions-typed', {
+        "strings": [
+            "issues?",
+            "problems?",
+            "questions?",
+            "ideas?",
+            "concerns?",
+            "doubts?",
+            "suggestions?",
+            "inquiries?",
+            "thoughts?",
+            "hurdles?"
+        ],
+        typeSpeed: 90,
+        backDelay: 2000,
+        backSpeed: 70,
+        showCursor: false,
+        shuffle: true,
+        loop: true,
+        loopCount: Infinity,
+    });
+    
+    
+    // animations
+    ScrollReveal({ distance: '15px', reset: true });
+    
+    const scrollRevealConfigs = [
+        { selector: '.navbar', options: { delay: 100, duration: 500, origin: 'top' } },
+        { selector: '#hero-image', options: { delay: 400, duration: 500, origin: 'right' } },
+        { selector: '#hero-text', options: { delay: 700, duration: 800, origin: 'left' } },
+        { selector: '#mockup-image', options: { delay: 400, duration: 500, scale: 0.2 } },
+        { selector: '#mockup-text', options: { delay: 700, duration: 1200, origin: 'right' } },
+        { selector: '#install', options: { delay: 300, duration: 500, scale: 0 } },
+        { selector: '#discord', options: { delay: 300, duration: 500, origin: 'bottom' } },
+        { selector: '#macbook_mockup_discord', options: { delay: 800, duration: 500 } },
+        { selector: '.footer', options: { delay: 100, duration: 500, origin: 'bottom' } }
+    ];
+    
+    scrollRevealConfigs.forEach(config => {
+        ScrollReveal().reveal(config.selector, config.options);
+    });
 });
--- readme.md
+++ readme.md
@@ -7,5 +7,6 @@ this webpage describes in short words the features of our app.
 - [our documentation page](https://docs.screenie.host)
 
 animations are made with [scrollreveal](https://scrollrevealjs.org/)
+text typing animations are made with [typed.js](https://github.com/mattboldt/typed.js)
 
 thank you for your patience and support!
